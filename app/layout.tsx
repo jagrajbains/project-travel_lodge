536271e9551649658123f33b681dba08dad24cab
jagrajbains <jagrajbains.94@gmail.com>
2023-07-09 23:46:33 +0530

    Added Header UI (Modal, Input, Button, zustand) and prisma config with mongodb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
-import Navbar from './components/navbar/Navbar'
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import RegisterModal from "./components/modals/RegisterModal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
+import ClientOnly from "./components/ClientOnly";
 
-const nunito = Nunito({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Travel-Lodge',
-  description: 'Hotel booking app by Jagraj Bains',
-}
+  title: "Travel-Lodge",
+  description: "Hotel booking app by Jagraj Bains",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
-  )
+  );
 }
